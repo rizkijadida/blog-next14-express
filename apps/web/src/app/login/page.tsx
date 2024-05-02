0b34e38e0d24adfeda6dab9aa608424e3bbc94c9
rizkijadida <rizkijadida@gmail.com>
2024-05-02 10:36:21 +0700

    feat: add login

---
+++ apps/web/src/app/login/page.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import FormInput from '@/components/FormInput';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { useFormik } from 'formik';
+import { validationSchema } from './validationShema';
+import useLogin from '@/hooks/api/auth/useLogin';
+
+const Login = () => {
+  const { login } = useLogin();
+  const { values, errors, touched, handleChange, handleBlur, handleSubmit } =
+    useFormik({
+      initialValues: {
+        fullName: '',
+        email: '',
+        password: '',
+      },
+      validationSchema,
+      onSubmit: (values) => {
+        login(values);
+      },
+    });
+  return (
+    <main className="container mx-auto h-[90vh] px-4">
+      <div className="mt-40 flex justify-center">
+        <Card className="w-[450px] ">
+          <CardHeader>
+            <CardTitle className="text-center text-3xl text-primary ">
+              Login
+            </CardTitle>
+            WELOME TO BLOG
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              <div className="grid w-full items-center gap-4">
+                <FormInput
+                  name="email"
+                  type="text"
+                  label="email"
+                  placeholder="Email"
+                  value={values.email}
+                  error={errors.email}
+                  isError={!!touched.email && !!errors.email}
+                  handleChange={handleChange}
+                  handleBlur={handleBlur}
+                />
+
+                <FormInput
+                  name="password"
+                  type="password"
+                  label="password"
+                  placeholder="password"
+                  value={values.password}
+                  error={errors.password}
+                  isError={!!touched.password && !!errors.password}
+                  handleChange={handleChange}
+                  handleBlur={handleBlur}
+                />
+
+                <Button type="submit" className=" mt-6 w-full text-white">
+                  Login
+                </Button>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex justify-end">
+            {/* <Button className=" text-white">Register</Button> */}
+          </CardFooter>
+        </Card>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
